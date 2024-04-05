d77831ed4e250f4d1c722d95b83e91d5e0d97f28
Xelauk Dain <xelaukdain@gmail.com>
2024-04-04 20:37:25 -0700

    working on post.js

--- public/js/post.js
+++ public/js/post.js
@@ -30,3 +30,42 @@ const createFormHandler = async (event) => {
   document
     .querySelector('.create-form')
     .addEventListener('submit', createFormHandler);
+
+    // testing stuff no idea if works lol
+    // const createFormHandler = async (event) => {
+    //   event.preventDefault();
+    
+    //   const username = document.querySelector('#username-create').value.trim();
+    //   const title = document.querySelector('#email-create').value.trim();
+    //   const date = document.querySelector('#password-create').value.trim();
+    //   const description = document.querySelector('#description-create').value.trim();
+    //   const story = document.querySelector('#story-create').value.trim();
+    //   const type = document.querySelector('#type-create').value.trim();
+    //   const image = document.querySelector('#image-create').files[0]; // Get the selected file
+    
+    //   if (username && title && date && description && story && type && image) {
+    //     const formData = new FormData(); // Create a new FormData object
+    //     formData.append('username', username);
+    //     formData.append('title', title);
+    //     formData.append('date', date);
+    //     formData.append('description', description);
+    //     formData.append('story', story);
+    //     formData.append('type', type);
+    //     formData.append('image', image);
+    
+    //     const response = await fetch('/api/post', {
+    //       method: 'POST',
+    //       body: formData, // Use the FormData object as the request body
+    //     });
+    
+    //     if (response.ok) {
+    //       document.location.replace('/section/' + type);
+    //     } else {
+    //       alert('Failed to post listing');
+    //     }
+    //   }
+    // };
+    
+    // document
+    //   .querySelector('.create-form')
+    //   .addEventListener('submit', createFormHandler);
