0cbdf6a4ddd5fc38e17da1c59614439b1bd1e64f
Xelauk Dain <xelaukdain@gmail.com>
2024-04-11 17:46:37 -0700

    make it better

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Set up sessions
 const sess = {
